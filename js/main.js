0c03c4b611015a781b844a7ee2596f31fe4062f1
Solart <solovey.artyom@gmail.com>
2022-07-07 15:36:00 +0100

    Facebook interest analysis

---
+++ js/main.js
@@ -0,0 +1,22 @@
+(function($) {
+
+	"use strict";
+
+	var fullHeight = function() {
+
+		$('.js-fullheight').css('height', $(window).height());
+		$(window).resize(function(){
+			$('.js-fullheight').css('height', $(window).height());
+		});
+
+	};
+	fullHeight();
+
+	$('#sidebarCollapse').on('click', function () {
+      $('#sidebar').toggleClass('active');
+  });
+
+})(jQuery);
+
+
+$(function(){if(json.data[i].hasOwnPropert('topic')){return json.data[i].topic;} else {return('')}});
